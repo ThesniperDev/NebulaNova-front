870696974e7315c6ffa5ee8ce43487fded466945
AiramGG <airamguedes99@gmail.com>
2024-04-08 16:49:33 +0100

    - Test Done, NavBar for mobile. - Needs minor fixes

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -1,52 +1,57 @@
 import './Header.css'
 
+import { useState } from 'react'
+
 import {
   AppBar,
-  Box,
-  Button,
   Toolbar,
   Typography,
-  Container,
+  IconButton,
+  Menu as CardMenu,
+  MenuItem
 } from '@mui/material'
 
-const settings = ['Log In', 'Sign Up'];
-const pages = ['Home', 'Games', 'Reviews']
+import { Menu as MenuIcon } from '@mui/icons-material'
 
 const Header = () => {
-  return (
-    <AppBar position="static">
-      <Container sx={{ padding: '15px 0', width: '100%' }}>
-        <Toolbar sx={{ width: '100%', display: 'flex' }}>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
-          <img src='https://media.discordapp.net/attachments/1214207531409473588/1226834656549408778/logooo.png?ex=662635cf&is=6613c0cf&hm=7b93a80e55d73bf74f809479bdfab05ed5979823cf11c4adc440dbba830fb060&=&format=webp&quality=lossless' className='img'/>
-          <Typography variant='h1'>
-            NebulaNova
-          </Typography>
-        </Box>
-        <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-          <Box sx={{ flexGrow: 0, display: 'flex', justifyContent: 'flex-end' }}>
-            {settings.map((setting) => (
-              <Button
-              key={setting}
-              sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {setting}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
-};
+  const [anchorEl, setAnchorEl] = useState(null)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-export default Header;
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+    setIsMenuOpen(true)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+    setIsMenuOpen(false)
+  }
+
+  return (
+    <AppBar position="static" color="secondary">
+      <Toolbar variant="dense">
+        <IconButton
+          onClick={handleClick}
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 10 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <CardMenu open={isMenuOpen} anchorEl={anchorEl} onClose={handleClose}>
+          <MenuItem>Home</MenuItem>
+          <MenuItem>Games</MenuItem>
+          <MenuItem>Reviews</MenuItem>
+          <MenuItem>Log In</MenuItem>
+          <MenuItem>Sign Up</MenuItem>
+        </CardMenu>
+        <Typography variant='h1' color='white' component='div' sx={{ display: 'flex', alignItems: 'center', gap: '5px', fontWeight: 'bold' }}>
+        <img src='https://media.discordapp.net/attachments/1214207531409473588/1226834656549408778/logooo.png?ex=662635cf&is=6613c0cf&hm=7b93a80e55d73bf74f809479bdfab05ed5979823cf11c4adc440dbba830fb060&=&format=webp&quality=lossless' className='img'/>
+          NebulaNova
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header
