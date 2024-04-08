0d62dd2ccbde7e22b8f1fdc718c1b0b7ea30938c
Aythami Báez <aythami.86@gmail.com>
2024-04-08 16:39:45 +0100

    REVIEWS : - Created component - Added routes

--- front-end/src/router/router.jsx
+++ front-end/src/router/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home/Home";
+import Review from "../components/Review/Review"
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,9 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+      }, {
+        path: "/review",
+        element: <Review />,
       },
     ],
   },
