e57307444438ff498d9bd2e73fd9f7b18e53ec75
Aythami Báez <aythami.86@gmail.com>
2024-04-08 09:58:58 +0100

    LOGIN: -  Deleted folder services

--- front-end/src/pages/Login/Login.jsx
+++ front-end/src/pages/Login/Login.jsx
@@ -11,9 +11,7 @@ import {
 } from "@mui/material";
 import { Lock, Email, Visibility, VisibilityOff } from "@mui/icons-material";
 
-import { useState } from "react";
-//import { login } from '../../services/auth'
-
+import { useState } from "react"
 const Login = () => {
   const [isPassVisible, setIsPassVisible] = useState(false);
 
@@ -26,8 +24,8 @@ const Login = () => {
   const [password, setPassword] = useState("")
 
   const validateEmail = ( email ) => {
-    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return regex.test(email)
+    const REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return REGEX.test(email)
 
   }
 
--- front-end/src/services/auth.js
+++
@@ -1,6 +0,0 @@
-import { api } from './serviceConfig'
-
-export const login = async ( userLogin) {
-    const { data } = await api.post('/login', userLogin)
-    return data
-}
