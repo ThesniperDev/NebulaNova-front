95db6685a906d4f8676905f2382f328c0221eb29
AiramGG <airamguedes99@gmail.com>
2024-04-08 14:23:18 +0100

    - Header and Footer done (Header needs a new fix)

--- front-end/src/components/Footer/Footer.jsx
+++ front-end/src/components/Footer/Footer.jsx
@@ -1,8 +1,69 @@
+import './Footer.css'
+
+import {
+  Box,
+  Container,
+  Typography,
+  Button,
+  Grid
+} from '@mui/material'
+
+import InstagramIcon from '@mui/icons-material/Instagram'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import XIcon from '@mui/icons-material/X'
+
+
 const Footer = () => {
+  const elements = [
+    {
+      header: 'Important Information',
+      links: ['Privacy Policy', 'Cookie Policy', 'Legal Advice']
+    },
+    {
+      header: 'Follow Our Social Networks',
+      links: ['NebulaNova', 'NebulaNova', 'NebulaNova']
+    }
+]
+
+  const generateFooterElements = () =>  {
+    const footerElements = elements.map((column, i) => {
+      return (
+        <Grid item md={3} key={i}>
+          <Box>
+            <Button sx={{color: 'white', fontWeight: 'bold' }}>
+              {column.header}
+            </Button>
+          </Box>
+            {column.links.map((link, idx) => {
+              return (
+                <Box key={idx}>
+                  <Button sx={{ color: 'white' }}>
+                    {link}
+                  </Button>
+                </Box>
+              )
+            })}
+        </Grid>
+      )
+    })
+    return footerElements
+  }
+
   return (
-    <footer>
-      <h1>Footer</h1>
-    </footer>
+    <>
+      <Box sx={{ display: 'flex', width: '100vw', backgroundColor: '#353941'}}>
+        <Container sx={{ textAlign: 'center', padding: '15px 0'}}>
+          <Grid container columnSpacing={2} sx={{ justifyContent: 'space-evenly' }}>
+            {generateFooterElements()}
+          </Grid>
+        </Container>
+      </Box>
+      <Box sx={{ textAlign: 'center', backgroundColor: '#353941', paddingBottom: '15px' }}>
+        <Typography sx={{ color: 'white' }}>
+          © NebulaNova 2024 - All Rights Reserved
+        </Typography>
+      </Box>
+    </>
   );
 };
 
