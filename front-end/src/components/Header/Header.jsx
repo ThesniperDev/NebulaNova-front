2c90981217409e0b340848316bed71fd8690f7ff
AiramGG <airamguedes99@gmail.com>
2024-04-09 10:25:35 +0100

    Minor changes on header.jsx and .css

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -21,6 +21,7 @@ const Header = () => {
     setAnchorEl(event.currentTarget)
     setIsMenuOpen(true)
   }
+
   const handleClose = () => {
     setAnchorEl(null)
     setIsMenuOpen(false)
@@ -28,24 +29,25 @@ const Header = () => {
 
   return (
     <AppBar position="static" color="secondary">
-      <Toolbar variant="dense">
+      <Toolbar variant="dense" sx={{ display: 'flex' }}>
         <IconButton
           onClick={handleClick}
           edge="start"
           color="inherit"
           aria-label="menu"
-          sx={{ mr: 10 }}
+          sx={{ mr: 10, display: 'flex-start' }}
         >
-          <MenuIcon />
+          <MenuIcon/>
         </IconButton>
-        <CardMenu open={isMenuOpen} anchorEl={anchorEl} onClose={handleClose}>
+        <CardMenu open={isMenuOpen} anchorEl={anchorEl} onClose={handleClose} sx={{ display: 'flex' }} >
           <MenuItem>Home</MenuItem>
           <MenuItem>Games</MenuItem>
           <MenuItem>Reviews</MenuItem>
+          <Typography sx={{ display: 'flex', justifyContent: 'center', color: 'gray' }}>Account</Typography>
           <MenuItem>Log In</MenuItem>
           <MenuItem>Sign Up</MenuItem>
         </CardMenu>
-        <Typography variant='h1' color='white' component='div' sx={{ display: 'flex', alignItems: 'center', gap: '5px', fontWeight: 'bold' }}>
+        <Typography className='logo' variant='h1' color='white' sx={{ gap: '5px', fontWeight: 'bold', margin: '5px 0' }}>
         <img src='https://media.discordapp.net/attachments/1214207531409473588/1226834656549408778/logooo.png?ex=662635cf&is=6613c0cf&hm=7b93a80e55d73bf74f809479bdfab05ed5979823cf11c4adc440dbba830fb060&=&format=webp&quality=lossless' className='img'/>
           NebulaNova
         </Typography>
