d5a7f6838940b587afbf888a26cb659ea4bc92c5
AiramGG <airamguedes99@gmail.com>
2024-04-10 13:46:49 +0100

    - NavBar when you are not logged done

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -1,169 +1,3 @@
-/* import './Header.css'
-
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-
-
-const pages = ['Home', 'Games', 'Reviews'];
-const settings = ['Log In', 'Sign Up'];
-
-const Header = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-  return (
-        <AppBar position="static" sx={{ backgroundColor: '#353941' }}>
-        <Container maxWidth="100%" sx={{ marginLeft: 0, marginRight: 0 }}>
-        <Toolbar disableGutters>
-          <Typography
-            variant="h1"
-            noWrap
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              color: 'inherit',
-              textDecoration: 'none',
-              padding: '15px 0',
-              alignItems: 'center',
-              gap: '5px'
-            }}
-          >
-            <img src='https://media.discordapp.net/attachments/1214207531409473588/1226834656549408778/logooo.png?ex=662635cf&is=6613c0cf&hm=7b93a80e55d73bf74f809479bdfab05ed5979823cf11c4adc440dbba830fb060&=&format=webp&quality=lossless' className='img'/>
-            NebulaNova
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center" sx={{ display: 'flex', flexGrow: 1 }}>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h1"
-            noWrap
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontWeight: 700,
-              color: 'inherit',
-              textDecoration: 'none',
-              padding: '15px 0',
-              alignItems: 'center',
-              gap: '5px'
-            }}
-          >
-            <img src='https://media.discordapp.net/attachments/1214207531409473588/1226834656549408778/logooo.png?ex=662635cf&is=6613c0cf&hm=7b93a80e55d73bf74f809479bdfab05ed5979823cf11c4adc440dbba830fb060&=&format=webp&quality=lossless' className='img'/>
-            NebulaNova
-          </Typography>
-          <Box sx={{ flexGrow: 1, padding: '10px 0', marginRight: '260.05px', display: { xs: 'none', md: 'flex' }, justifyContent: 'center' }}>
-            {pages.map((page) => (
-              <Button variant='h1'
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <AccountCircleIcon fontSize='large' sx={{ color: 'white' }}/>
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-      </Toolbar>
-      </Container>
-      </AppBar>
-      
-  );
-}
-export default Header;
- */
-
 import './Header.css'
 
 import * as React from 'react';
@@ -188,6 +22,7 @@ import MenuItem from '@mui/material/MenuItem';
 import Tooltip from '@mui/material/Tooltip';
 
 
+const pages = ['Home', 'Games', 'Reviews'];
 const settings = ['Log In', 'Sign Up'];
 
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -210,13 +45,17 @@ const Header = () => {
     setOpen(false);
   };
 
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
 
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#353941'}}>
-      <Container sx={{ marginLeft: 0, marginRight: 0, padding: 0, width: '100%' }}>
+    <AppBar position="static" sx={{ backgroundColor: '#353941' }}>
+      <Container sx={{ width: '100%' }}>
       <Toolbar disableGutters>
           <Typography
             variant="h1"
@@ -235,6 +74,18 @@ const Header = () => {
             <img src='https://media.discordapp.net/attachments/1214207531409473588/1226834656549408778/logooo.png?ex=662635cf&is=6613c0cf&hm=7b93a80e55d73bf74f809479bdfab05ed5979823cf11c4adc440dbba830fb060&=&format=webp&quality=lossless' className='img'/>
             NebulaNova
           </Typography>
+
+          <Box sx={{ flexGrow: 1, padding: '10px 0', marginRight: '260.05px', display: { xs: 'none', md: 'flex' }, justifyContent: 'center' }}>
+            {pages.map((page) => (
+              <Button variant='h1'
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
 
       <Button onClick={handleClickOpen} sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, justifyContent: 'flex-start' }} >
         <IconButton sx={{ color: 'white' }}>
@@ -319,9 +170,11 @@ const Header = () => {
             NebulaNova
           </Typography>
 
+
+
           <Box sx={{ display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end' }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0, display: 'flex', justifyContent: 'flex-end', marginRight: '5px' }}>
+            <Tooltip title="Open settings" sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <IconButton onClick={handleOpenUserMenu} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
                 <AccountCircleIcon fontSize='large' sx={{ color: 'white' }}/>
               </IconButton>
             </Tooltip>
